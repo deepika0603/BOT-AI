7477464e570affc80c1b7857d3b0aa270d330f8d
deepika <deepika75013@gmail.com>
2025-07-15 15:05:03 +0530

    implement Navbar

--- BOT-AI/src/main.jsx
+++ BOT-AI/src/main.jsx
@@ -1,11 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import History from './pages/History/History';
+import Home from './pages/Home/Home';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "history",
+        element: <History />
+      },
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
+  }
+])
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ BOT-AI/src/pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import NavBar from "../../components/Navbar/Navbar"
+import { useContext } from 'react';
+import { ThemeContext } from '../../theme/ThemeContext';
+
+
+export default function Home() {
+    const { mode } = useContext(ThemeContext);
+    return (
+       
+            <Stack
+            height={'100vh'}
+            justifyContent={'space-between'}
+            sx={{
+                '@media (max-width:767px)': {
+                    background: mode == 'light' ? 'linear-gradient(#F9FAFA 60%, #EDE4FF)' : ''
+                }
+            }}
+        >
+            <NavBar />
+        </Stack>
+           
+      
+    )
+}
